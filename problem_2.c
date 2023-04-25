7b5b8b20db94d784ee6154a7766125e9f06d9bd6
srayo16 <srayosikder@gmail.com>
2023-04-25 21:27:12 +0600

    push

--- problem_2.c
+++ problem_2.c
@@ -14,7 +14,23 @@
 int main()
 {   
     
-     
+     int n;
+     scanf("%d", &n);
+    int a[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //    scanf("%d", &a[i]);
+    // }
+    scanf("%s", a);
+    // printf("%s", a);
+    int sum=0;
+
+    for (int i = 0; i < n; i++)
+    {
+       sum= sum +a[i];
+    }
+    printf("%d", sum);
+    
 
     return 0;
 }
